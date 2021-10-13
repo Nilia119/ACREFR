03b7416d67ad416fd709c0f5f6297e8c45ed23ba
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-10-13 16:38:58 +0200

    Update cfgPatches.hpp

--- Addons/ACREFR_Backpacks/cfgPatches.hpp
+++ Addons/ACREFR_Backpacks/cfgPatches.hpp
@@ -1,1 +1,15 @@
+class ACREFR_Core
+	{
+		units[] = //Nichts solange cfgVehicles.hpp in die Config.cpp geladen wird
+		{
+		};
+		weapons[] = //Nichts solange cfgWeapons.hpp in die Config.cpp geladen wird
+		{
+		};
+		requiredVersion = 0.1;
+		requiredAddons[] = //Alle pbos auf denen die Inhallte basieren
+		{
+			"A3_Characters_F",
+		};
+	};
 
