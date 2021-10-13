e544a42e2717dfd7b7d188b2c8f8bb8f40b5cee1
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-10-13 13:56:12 +0200

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,18 +1,12 @@
-class CfgMods {
-  
-  class ACREFR {
-    
-    logo = "\Core\Data\Logo.paa";  //Logo displayed in bottom left of main menu
-    logoOver = "\Core\Data\Logo.paa"; //Logo displayed in the mod menu when mod icon is clicked 
-    logoSmall = "\Core\Data\Logo.paa"; //Logo displayed next to all units and vehicles in the editor 
+    logo = "Addons\Core\Data\Logo.paa";  //Logo displayed in bottom left of main menu
+    logoOver = "Addons\Core\Data\Logo.paa"; //Logo displayed in the mod menu when mod icon is clicked 
+    logoSmall = "Addons\Core\Data\Logo.paa"; //Logo displayed next to all units and vehicles in the editor 
     dlcColor[] = {0,0,0,1}; //Something about color
     hideName = 0; //Hide the name of the mod
     hidePicture = 0; //Hide the logo of the mod
     tooltipOwned = "ACRE Force Radio"; //Tooltip when mod icon is moused over in main menu
     name = "ACRE Force Radio"; //Full name of the mod 
     overview = "All TFAR Backpacks edited, without their original function,  to Work with ACRE 2"; //Description of the mod displayed in the "DLC" page of the main menu 
+    actionName = "Workshop";
     action = "http://steamcommunity.com/"; //Link to take people to from "Website" button
-    
-  };
-  
-};
+    author = "[9.JgKp] Nilia";
