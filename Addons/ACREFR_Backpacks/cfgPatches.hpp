108aa7855a0370c508570d994795fbc2036a25b5
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-10-15 13:12:51 +0200

    Update cfgPatches.hpp

--- Addons/ACREFR_Backpacks/cfgPatches.hpp
+++ Addons/ACREFR_Backpacks/cfgPatches.hpp
@@ -1,15 +1,35 @@
-class ACREFR_Core
-	{
-		units[] = //Nichts solange cfgVehicles.hpp in die Config.cpp geladen wird
-		{
-		};
-		weapons[] = //Nichts solange cfgWeapons.hpp in die Config.cpp geladen wird
-		{
-		};
-		requiredVersion = 0.1;
-		requiredAddons[] = //Alle pbos auf denen die Inhallte basieren
-		{
-			"A3_Characters_F",
-		};
-	};
+class CfgPatches {
+  class ACREFR_Backpacks {
+      units[] = {			//Nichts solange cfgVehicles.hpp in die Config.cpp geladen wird
+        "ACREFR_NATO_Radio_Crate",
+        "ACREFR_EAST_Radio_Crate",
+        "ACREFR_IND_Radio_Crate",
+        "ACREFR_rt1523g",
+        "ACREFR_anprc155",
+        "ACREFR_mr3000",
+        "ACREFR_mr3000_multicam",
+        "ACREFR_anprc155_coyote",
+        "ACREFR_rt1523g_sage",
+        "ACREFR_rt1523g_green",
+        "ACREFR_rt1523g_fabric",
+        "ACREFR_rt1523g_big",
+        "ACREFR_rt1523g_black",
+        "ACREFR_rt1523g_big_bwmod",
+        "ACREFR_mr3000_bwmod",
+        "ACREFR_rt1523g_bwmod",
+        "ACREFR_mr3000_bwmod_tropen",
+        "ACREFR_rt1523g_big_bwmod_tropen",
+        "ACREFR_rt1523g_big_rhs",
+        "ACREFR_rt1523g_rhs",
+        "ACREFR_mr3000_rhs",
+        "ACREFR_bussole"
+    };
+    weapons[] = {};			 //Nichts solange cfgWeapons.hpp in die Config.cpp geladen wird
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {		 //Alle pbos auf denen die Inhallte basieren
 
+    };
+    Url = "";
+    VERSION_CONFIG;
+  };
+};
