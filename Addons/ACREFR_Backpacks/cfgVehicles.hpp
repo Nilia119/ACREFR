fe8cfb85728530196d9a367bc1beb3e368a489f2
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-10-15 12:47:30 +0200

    Update cfgVehicles.hpp

--- Addons/ACREFR_Backpacks/cfgVehicles.hpp
+++ Addons/ACREFR_Backpacks/cfgVehicles.hpp
@@ -1,1 +1,23 @@
+class CfgVehicles {
+    class ReammoBox;
+    class Item_Base_F;
+
+    class Bag_Base: ReammoBox {
+        tf_hasLRradio = 0;
+        tf_encryptionCode = "";
+        tf_range = 20000;
+    };
+    class TFAR_Bag_Base: Bag_Base {
+        tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+        tf_hasLRradio = 1;
+        scope = HIDDEN;
+        scopeCurator = HIDDEN;
+        #include "\z\tfar\addons\static_radios\edenAttributes.hpp"
+    };
+
+    #include "anprc155\CfgVehicles.hpp"
+    #include "bussole\CfgVehicles.hpp"
+    #include "mr3000\CfgVehicles.hpp"
+    #include "rt1523g\CfgVehicles.hpp"
+    #include "enoch\CfgVehicles.hpp"
 
