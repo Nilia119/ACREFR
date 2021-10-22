7a3a4d329c7c43c142f07e52c0a0747aef52fa5d
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-10-23 00:34:16 +0200

    Update cfgvehicles.hpp

--- Addons/ACREFR_Backpacks/anarc210/cfgvehicles.hpp
+++ Addons/ACREFR_Backpacks/anarc210/cfgvehicles.hpp
@@ -1,12 +1,12 @@
 class ACREFR_anarc210: ACREFR_Bag_Base {
-    scope = PUBLIC;
-    scopeCurator = PUBLIC;
-    displayName = CSTRING(ANARC210);
-    descriptionShort = CSTRING(ANARC210_Desc);
-    picture = QPATHTOF(anarc210\ui\anarc210_icon.paa);
-    model = QPATHTOF(models\TFR_BACKPACK);
+    scope = 2;
+    scopeCurator = 2;
+    displayName = "$STR_ACREFR_backpacks_ANARC210";
+    descriptionShort = "$STR_ACREFR_backpacks_ANARC210_Desc";
+    picture = "ACREFR_Backpacks\anarc210\ui\anarc210_icon.paa";
+    model = "ACREFR_Backpacks\anarc210\ui\anarc210.paa";
     hiddenSelections[] = {"camo"};
-    hiddenSelectionsTextures[] = {QPATHTOF(models\data\camo\backpack_mcam_co.paa)};
+    hiddenSelectionsTextures[] = {"ACREFR_Backpacks\models\data\camo\backpack_mcam_co.paa")};
     maximumLoad = 20;
     mass = 160;
 };
