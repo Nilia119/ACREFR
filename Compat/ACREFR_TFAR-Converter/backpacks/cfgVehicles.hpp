4d54ec852bc57dc388e1cbab224a781a968a1961
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-11-09 12:39:16 +0100

    Added MR6000l and RT1523G Compatibility

--- Compat/ACREFR_TFAR-Converter/backpacks/cfgVehicles.hpp
+++ Compat/ACREFR_TFAR-Converter/backpacks/cfgVehicles.hpp
@@ -59,3 +59,67 @@ class TFAR_mr3000_multicam: ACREFR_mr3000_multicam {
   scopeCurator = 0;
   scopeArsenal = 0;  
 };
+//MR-6000l
+
+class TFAR_mr6000l: ACREFR_mr6000l {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+//RT-1523g
+
+class TFAR_rt1523g: ACREFR_rt1523g {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_rt1523g_bwmod: ACREFR_rt1523g_bwmod {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_rt1523g_rhs: ACREFR_rt1523g_rhs {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_rt1523g_big: ACREFR_rt1523g_big {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_rt1523g_big_bwmod: ACREFR_rt1523g_big_bwmod {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_rt1523g_big_bwmod_tropen: ACREFR_rt1523g_big_bwmod_tropen {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_rt1523g_big_rhs: ACREFR_rt1523g_big_rhs {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_rt1523g_sage: ACREFR_rt1523g_sage {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_rt1523g_green: ACREFR_rt1523g_green {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_rt1523g_fabric: ACREFR_rt1523g_fabric {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_rt1523g_black: ACREFR_rt1523g_black {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
