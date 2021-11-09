bd86a00ae8082064f287194872ce42448466746b
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-11-09 10:57:29 +0100

    Added first Compatibility

--- Compat/ACREFR_TFAR-Converter/backpacks/cfgVehicles.hpp
+++ Compat/ACREFR_TFAR-Converter/backpacks/cfgVehicles.hpp
@@ -1,1 +1,61 @@
+//Replaces old TFAR Classes with ACREFR Classes
+//ANARC-164
 
+class TFAR_anarc164: ACREFR_anarc164 {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;
+};
+//ANARC-210
+
+class TFAR_anarc210: ACREFR_anarc210 {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+//ANARC-155
+
+class TFAR_anarc155: ACREFR_anarc155 {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_anarc155_coyote: ACREFR_anarc155_coyote {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+//Bussole
+
+class TFAR_bussole: ACREFR_bussole {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+//MR-3000
+
+class TFAR_mr3000: ACREFR_mr3000 {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_mr3000_bwmod: ACREFR_mr3000_bwmod {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_mr3000_bwmod_tropen: ACREFR_mr3000_bwmod_tropen {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_mr3000_rhs: ACREFR_mr3000_rhs {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
+class TFAR_mr3000_multicam: ACREFR_mr3000_multicam {
+  scope= 0;
+  scopeCurator = 0;
+  scopeArsenal = 0;  
+};
