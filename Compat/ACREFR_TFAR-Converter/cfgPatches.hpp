fe2930e2afa964d6cd763c70778884871824f30d
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-11-09 10:39:19 +0100

    Added cfgPatches.hpp

---
+++ Compat/ACREFR_TFAR-Converter/cfgPatches.hpp
@@ -0,0 +1,20 @@
+class CfgPatches {
+  class ACREFR_Backpacks {
+	  name = "ACREFR - Backpacks";	  
+	  author = "Nilia";
+	  version = "0.0.1";
+	  versionStr = "0.0.1";
+	  versionAr[] = {0,0,1};
+    units[] = {			//Nichts solange cfgVehicles.hpp in die Config.cpp geladen wird };
+    weapons[] = {};			 //Nichts solange cfgWeapons.hpp in die Config.cpp geladen wird
+    requiredVersion = "";
+		requiredAddons[] = {		 
+		
+		"ACREFR_Core",
+    "ACREFR_Backpacks"
+     
+		
+		};	//Alle pbos auf denen die Inhallte basieren
+    Url = "https://github.com/Nilia119/ACREFR";
+  };
+};
