02cc38b0474f181dc77b843be5234b1094a26620
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-11-10 10:53:31 +0100

    Included Ammobox.hpp

--- Addons/ACREFR_Backpacks/cfgVehicles.hpp
+++ Addons/ACREFR_Backpacks/cfgVehicles.hpp
@@ -1,4 +1,5 @@
 class CfgVehicles {
+    #include "Ammobox.hpp"
     class ReammoBox;
     class Item_Base_F;
 
@@ -11,11 +12,11 @@ class CfgVehicles {
     };
 
     #include "anprc155\CfgVehicles.hpp"
-	#include "anarc164\CfgVehicles.hpp"
-	#include "anarc210\CfgVehicles.hpp"
+    #include "anarc164\CfgVehicles.hpp"
+    #include "anarc210\CfgVehicles.hpp"
     #include "bussole\CfgVehicles.hpp"
     #include "mr3000\CfgVehicles.hpp"
-	#include "mr6000l\CfgVehicles.hpp"
+    #include "mr6000l\CfgVehicles.hpp"
     #include "rt1523g\CfgVehicles.hpp"
   
 };
